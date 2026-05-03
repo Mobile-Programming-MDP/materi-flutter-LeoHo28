022a6f9865e1678a731945c7aff3af4df13b5fcd
leo <leonardoho_2428250030@mhs.mdp.ac.id>
2026-05-03 17:54:42 +0700

    add,detail

---
+++ CEPU_APP/cepu_app/lib/screens/home_screen.dart
@@ -0,0 +1,110 @@
+import 'package:cepu_app/screens/add_post_screen.dart';
+import 'package:cepu_app/screens/sign_in_screen.dart';
+import 'package:cepu_app/service/post_service.dart';
+import 'package:cepu_app/widgets/post_list_item.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+    if (!mounted) return;
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => SignInScreen()),
+      (route) => false,
+    );
+  }
+
+  //Fungsi untuk membuat url foto profile / avatar
+  String generateAvatarUrl(String? fullName) {
+    final formattedName = fullName!.trim().replaceAll(' ', '+');
+    return 'https://ui-avatars.com/api/?name=$formattedName&color=FFFFFF&background=000000';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentUserId = FirebaseAuth.instance.currentUser?.uid;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              signOut();
+            },
+            icon: Icon(Icons.logout),
+            tooltip: "Sign Out",
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 8.0),
+          Image.network(
+            generateAvatarUrl(
+              FirebaseAuth.instance.currentUser?.displayName.toString(),
+            ),
+            width: 80,
+            height: 80,
+          ),
+          const SizedBox(height: 8.0),
+          Text(
+            FirebaseAuth.instance.currentUser!.displayName!,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8.0),
+          const Divider(),
+          Expanded(
+            child: StreamBuilder(
+              stream: PostService.getPostList(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+                final posts = snapshot.data ?? [];
+                if (posts.isEmpty) {
+                  return const Center(child: Text('No posts yet.'));
+                }
+                return RefreshIndicator(
+                  onRefresh: () async {
+                  },
+                  child: ListView.builder(
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      final post = posts[index];
+                      final isOwner =
+                          currentUserId != null &&
+                          post.userId == currentUserId;
+      //Buat widget PostListItem, di dalam folder widgets 
+      //dengan nama file post_list_item.dart
+                      return PostListItem(post: post, isOwner: isOwner);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const AddPostScreen()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
